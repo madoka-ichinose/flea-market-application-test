8a35b5f9ee05bbaf7ab36958a92fb1ff829ffb3e
madoka ichinose <madoka.shinji.1208@gmail.com>
2025-05-30 12:41:44 +0900

    修正版5/30

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(ProductsTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
     }
 }
